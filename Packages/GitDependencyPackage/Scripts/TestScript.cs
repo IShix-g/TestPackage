a080eee4739ad47d607bdbf87f82aaf35ac33202
IShix <49128115+IShix-g@users.noreply.github.com>
2024-12-02 23:14:04 +0900

    feat: Add new test packages

---
+++ Packages/GitDependencyPackage/Scripts/TestScript.cs
@@ -0,0 +1,8 @@
+
+namespace TestGitDependencyPackage.Scripts
+{
+    public sealed class TestScript
+    {
+        public string Message => "Hello World! from Test git dependency package";
+    }
+}
---
+++ Packages/UnityDependencyPackage/Scripts/TestScript.cs
@@ -0,0 +1,8 @@
+
+namespace TestUnityDependencyPackage.Scripts
+{
+    public sealed class TestScript
+    {
+        public string Message => "Hello World! from Test unity dependency package";
+    }
+}
