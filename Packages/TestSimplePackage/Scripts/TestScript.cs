6bd13496930596699973e7373ea951c7d07d996f
IShix-g <49128115+IShix-g@users.noreply.github.com>
2025-01-19 02:00:23 +0900

    feat: add new TestSimplePackage package

---
+++ Packages/TestSimplePackage/Scripts/TestScript.cs
@@ -0,0 +1,9 @@
+
+namespace TestSimplePackage.Scripts
+{
+    public sealed class TestScript
+    {
+        public string Message => "Hello World! from Test simple package";
+        public string Message2 => "Hello World! 2 from Test simple package";
+    }
+}
