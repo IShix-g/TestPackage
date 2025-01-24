89997827d17b1659f7d2bdf1371bb8349e65e10a
IShix <49128115+IShix-g@users.noreply.github.com>
2025-01-24 12:25:06 +0900

    Remove unused properties from TestScript class

--- Packages/SimplePackage/Scripts/TestScript.cs
+++ Packages/SimplePackage/Scripts/TestScript.cs
@@ -4,7 +4,5 @@ namespace SimplePackage.Scripts
     public sealed class TestScript
     {
         public string Message => "Hello World! from Test simple package";
-        public string Message2 => "Hello World! from Test simple package 2";
-        public string Message3 => "Hello World! from Test simple package 3";
     }
 }
