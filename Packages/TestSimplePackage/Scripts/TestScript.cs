23b48fe7a7c8119067b20ad56475e7633c02dfd8
IShix <49128115+IShix-g@users.noreply.github.com>
2025-11-25 13:08:38 +0900

    feat: Add timeline settings asset and message printing functionality to test script

--- Packages/TestSimplePackage/Scripts/TestScript.cs
+++ Packages/TestSimplePackage/Scripts/TestScript.cs
@@ -1,9 +1,13 @@
 
+using UnityEngine;
+
 namespace TestSimplePackage.Scripts
 {
     public sealed class TestScript
     {
         public string Message => "Hello World! from Test simple package";
         public string Message2 => "Hello World! 2 from Test simple package";
+        
+        public void PrintMessage() => Debug.Log(Message);
     }
 }
